9b515b9a16416de79b1ca9066ec0acb50d389bb3
장우석 <cyj2651@naver.com>
2018-11-02 04:14:52 +0900

    패턴1 틀 완성
    좌우로 왔다갔다함

--- bullet.py
+++ bullet.py
@@ -1,7 +1,7 @@
 import game_framework
 from pico2d import *
 from ball import Ball
-#from math import *
+import math
 import random
 
 import game_world
@@ -14,8 +14,11 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-GHOST_ASSENTION_SPEED_CMPS = 30.0
-GHOST_TIME_PER_RADIAN = 4 * 3.141592 * 60
+
+# 내가 추가함
+PI = 3.141592
+PATTERN1_TIME_PER_RADIAN = 0.25 * PI
+
 # Boy Action Speed
 # fill expressions correctly
 TIME_PER_ACTION = 0.5
@@ -26,6 +29,7 @@ FRAMES_PER_ACTION = 8
 
 # Boy Event
 RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER, SPACE = range(6)
+PATTERN1_LEFT, PATTERN1_RIGHT = range(2)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
@@ -42,6 +46,12 @@ class IdleState:
 
     @staticmethod
     def enter(bullet, event):
+        global radian, radius, turnLR
+        bullet.x = 300
+        bullet.y = 800
+        radian = -1 * PI
+        radius = 0
+        turnLR = 1
         bullet.timer = get_time()
 
     @staticmethod
@@ -50,15 +60,31 @@ class IdleState:
             boy.fire_ball()
         pass
 
+
     @staticmethod
     def do(bullet):
+        global radian, radius, turnLR
+
+        if turnLR == 1:
+            if radian < -0.25 * PI:
+                radian += PATTERN1_TIME_PER_RADIAN * game_framework.frame_time
+            else:
+                turnLR = 0
+        elif turnLR == 0:
+            if radian > -0.75 * PI:
+                radian -= PATTERN1_TIME_PER_RADIAN * game_framework.frame_time
+            else:
+                turnLR = 1
+        radius += 1
         pass
 
 
 
     @staticmethod
     def draw(bullet):
-        bullet.image.draw(50, 50)
+        bullet.image.draw(bullet.x + radius * math.cos(radian), bullet.y + radius * math.sin(radian))
+
+
 
 
 
