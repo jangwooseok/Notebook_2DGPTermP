4e50d0f5c352f7ef6776c32e65619d72c58f7cef
장우석 <cyj2651@naver.com>
2018-11-23 01:22:39 +0900

    전체화면으로
    전체화면

--- mygame.py
+++ mygame.py
@@ -1,8 +1,11 @@
 import game_framework
 import pico2d
 
+import start_state
+
 import main_state
 
-pico2d.open_canvas(600, 1000, sync = True)
+pico2d.open_canvas(1920, 1000, True, True)
+game_framework.run(start_state)
 game_framework.run(main_state)
 pico2d.close_canvas()
