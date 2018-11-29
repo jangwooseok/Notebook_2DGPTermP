ee911d912c8a503ed2d5ecb66244e821d6aaf693
장우석 <cyj2651@naver.com>
2018-11-29 23:05:08 +0900

    스페이스를 눌러야 메인으로 감
    전체버튼 -> 스페이스

--- title_state.py
+++ title_state.py
@@ -8,16 +8,12 @@ name = "TitleState"
 image = None
 logo_time = 0.0
 
-KEY_DOWN = range(1)
 
-key_event_table = {
-    (SDL_KEYDOWN) : KEY_DOWN
-}
+
 
 
 def enter():
     global image, titleSound
-    global running
 
     running = True
     image = load_image('title.png')
@@ -38,7 +34,6 @@ def exit():
 
 def update():
     global logo_time
-    global running
 
 
     if (logo_time > 2.0):
@@ -58,16 +53,14 @@ def draw():
     update_canvas()
     pass
 
-next_state_table = {
-   KEY_DOWN: main_state
-}
-
 
 def handle_events():
     global running
     events = get_events()
     for event in events:
-        if event.type == SDL_KEYDOWN:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
             game_framework.change_state(main_state)
 
     pass
