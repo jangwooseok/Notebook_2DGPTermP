377d9ecae2e1db859941786ac9ee6879f7731dc7
장우석 <cyj2651@naver.com>
2018-11-30 14:19:01 +0900

    승리 화면 효과 추가

--- end_state_win.py
+++ end_state_win.py
@@ -5,16 +5,19 @@ from pico2d import *
 import main_state
 
 name = "winendState"
-image = None
+image_background = None
+image_bacteria = None
 logo_time = 0.0
 
-
+radian = 0
+size = 520
 
 
 
 def enter():
-    global image, Sound
-    image = load_image('end.png')
+    global image_background, image_bacteria, Sound
+    image_background = load_image('win.PNG')
+    image_bacteria = load_image('win_bacteria.PNG')
     Sound = load_music('ending.mp3')
     Sound.set_volume(64)
     Sound.repeat_play()
@@ -34,13 +37,22 @@ def exit():
 
 def update():
     global logo_time
+    global radian, size
+    radian += 0.01
+    size -= 5
+    if size <= 400:
+        size = 520
     pass
 
 
 def draw():
-    global image
+    global image_background, image_bacteria
+    global radian, size
+    #def clip_composite_draw(self, left, bottom, width, height, rad, flip, x, y, w = None, h = None):
+
     clear_canvas()
-    image.clip_draw(0, 0, 600, 1000, 600//2, 1000//2, 600, 1000)      #(self, left, bottom, width, height, x, y, w=None, h=None):
+    image_background.clip_draw(0, 0, 600, 1000, 600//2, 1000//2, 600, 1000)      #(self, left, bottom, width, height, x, y, w=None, h=None):
+    image_bacteria.clip_composite_draw(0, 0, 520, 520, radian, 'None', 600//2, 740, size, size)      #(self, left, bottom, width, height, x, y, w=None, h=None):
     update_canvas()
     pass
 
--- mygame.py
+++ mygame.py
@@ -11,8 +11,8 @@ import main_state
 
 pico2d.open_canvas(600, 1000, True, False)
 #game_framework.run(start_state)
-game_framework.run(title_state)
-game_framework.run(main_state)
+#game_framework.run(title_state)
+#game_framework.run(main_state)
 game_framework.run(end_state_win)
 game_framework.run(end_state_lose)
 pico2d.close_canvas()
